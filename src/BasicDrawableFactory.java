e51cef689ae6a5cdc2dc0e738bcf081b2338b313
emon100 <emon100@qq.com>
2020-11-26 22:26:13 +0800

    add test classes

---
+++ src/BasicDrawableFactory.java
@@ -0,0 +1,106 @@
+import java.awt.*;
+import java.awt.geom.*;
+
+
+public class BasicDrawableFactory {
+    private static class Line implements Drawable {
+        Line2D.Float line;
+        BasicStroke borderStroke;
+        float alpha= 1f;
+        COLOR color=COLOR.BLACK;
+
+        public Line(Point point) {
+            line = new Line2D.Float(point.x,point.y,point.x,point.y);
+            borderStroke = new BasicStroke();
+        }
+
+        @Override
+        public void drawOnGraphics2D(Graphics2D g) {
+            // initial g settings
+            g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, alpha)); //设置透明度
+            //
+            g.draw(line);
+
+        }
+
+        @Override
+        public void setAlpha(float f) {
+            if(f<=1 && f>=0){ //validate alpha value
+                alpha = f;
+            }
+        }
+
+        @Override
+        public float getAlpha() {
+            return alpha;
+        }
+
+        @Override
+        public void setColor(COLOR c) {
+            color = c;
+        }
+
+        @Override
+        public void setBorder(COLOR c, BasicStroke s) {
+            color = c;
+            borderStroke = s;
+        }
+
+        @Override
+        public Drawable.COLOR getBorderColor() {
+            return color;
+        }
+
+        @Override
+        public BasicStroke getBasicStroke() {
+            return borderStroke;
+        }
+
+        @Override
+        public void setFillColor(COLOR c) {
+            color = c;
+        }
+
+        @Override
+        public Drawable.COLOR getFillColor() {
+            return color;
+        }
+
+        @Override
+        public void scale(float times) {
+            // TODO Auto-generated method stub
+        }
+
+        @Override
+        public void MovestartTo(Point2D.Float p) {
+            line = new Line2D.Float(p.x,p.y,line.x2,line.y2);
+        }
+
+        @Override
+        public Point2D.Float getStart() {
+            return new Point2D.Float(line.x1,line.y1);
+        }
+
+        @Override
+        public void putEndPoint(Point2D.Float p) {
+            // TODO Auto-generated method stub
+        }
+
+        @Override
+        public Point2D.Float getEndPoint() {
+            // TODO Auto-generated method stub
+            return new Point2D.Float(line.x1,line.y1);
+        }
+
+        @Override
+        public boolean pointOn(Point2D.Float p) {
+            // TODO Auto-generated method stub
+            return false;
+        }
+
+    }
+    public static Drawable makeLine(int startx,int starty,int endx,int endy) {
+        return new Line(new Point(startx,starty));
+    }
+    
+}
--- src/Canvas.java
+++ src/Canvas.java
@@ -1,4 +1,6 @@
 import java.awt.*;
+import java.util.ArrayList;
+
 import javax.swing.*;
 
 public class Canvas extends JFrame {
@@ -15,7 +17,29 @@ public class Canvas extends JFrame {
     public Canvas() {
         this.setSize(300, 300);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.add(new PaintSurface(), BorderLayout.CENTER);
+        this.add(new PaintSurface(
+            new StatesModel(){
+                public void execute(Command command){}
+                public void undo(){}
+                public void redo(){}
+            
+                public ArrayList<Drawable> getAllDrawable(){
+                    var a = new ArrayList<Drawable>();
+                    a.add(BasicDrawableFactory.makeLine(1, 18,50,50));
+                    return a;
+                }
+                public Drawable.TYPE getType(){
+                    return Drawable.TYPE.LINE;
+                }
+                public Drawable.COLOR getColor(){
+                    return Drawable.COLOR.BLACK;
+                }
+                public String getStringInput(){
+                    return "TryString";
+                }
+                public void setDrawable(ArrayList<Drawable> d){}
+            }
+        ), BorderLayout.CENTER);
         this.setVisible(true);
     }
 }
