fa13b5f07f8546cde31dd3f82411bfdc4df60372
AlbertTan <669196427@qq.com>
2020-11-29 13:12:21 +0800

    add Chrome

--- src/Canvas.java
+++ src/Canvas.java
@@ -11,9 +11,59 @@ public class Canvas extends JFrame {
      */
     private static final long serialVersionUID = 1L;
 
+
+
     public static void main(String[] args) {
-        new Canvas();
+        Chrome chrome = new Chrome();
+        chrome.add(new PaintSurface(new StatesModel() {
+
+
+            public void execute(Command command) {
+            }
+
+            public void undo() {
+            }
+
+            public void redo() {
+            }
+
+            ArrayList<Drawable> a = new ArrayList<>();
+
+            {
+                var r  = new Random();
+                a.add(BasicDrawableFactory.makeLine(r.nextInt(200),r.nextInt(200),r.nextInt(200),r.nextInt(200)));
+            }
+
+            public ArrayList<Drawable> getAllDrawable() {
+                return a;
+            }
+
+            public Drawable.TYPE getType() {
+                return Drawable.TYPE.PATH;
+            }
+
+            public Color getColor() {
+                return Color.BLUE;
+            }
+
+            public String getStringInput() {
+                return "TryString";
+            }
+
+            public void setDrawable(ArrayList<Drawable> d) {
+                a = d;
+            }
+
+            @Override
+            public float getAlpha() {
+                // TODO Auto-generated method stub
+                return 1.0f;
+            }
+
+        }),new Chrome.GBC(0,3,15,7).setFill(Chrome.GBC.BOTH).setWeight(0,100));
+        chrome.setVisible(true);
     }
+
 
     public Canvas() {
         this.setSize(300, 300);
@@ -30,7 +80,7 @@ public class Canvas extends JFrame {
             public void redo() {
             }
 
-            ArrayList<Drawable> a = new ArrayList<Drawable>();
+            ArrayList<Drawable> a = new ArrayList<>();
 
             {
                 var r  = new Random();
