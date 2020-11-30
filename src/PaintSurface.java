8179c4429db6be0998856177768459f02eefaa54
emon100 <emon100@qq.com>
2020-11-30 19:37:23 +0800

    Add drag and move. Fix textbox.

--- src/PaintSurface.java
+++ src/PaintSurface.java
@@ -1,6 +1,7 @@
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.geom.*;
+
 import javax.swing.*;
 class PaintSurface extends JComponent {
 
@@ -8,7 +9,8 @@ class PaintSurface extends JComponent {
      *
      */
     private static final long serialVersionUID = 1L;
-
+    
+    final float[] dash1 = { 3.0f, 3.0f }; //虚线的实心空心设置
 
     Point startDrag, endDrag; // 鼠标起始点，终止点
     Dimension size = getSize(); // 当前窗口大小
@@ -16,6 +18,8 @@ class PaintSurface extends JComponent {
     StatesModel stm;// 程序中所有的的状态
 
     Drawable tmpDrawable = null; // 用于显示提示的临时Drawable对象
+    Drawable selectedTip = null; // 用于显示被选择对象外边框的临时Drawable对象
+    Drawable selectedDrawable = null; //被选择的对象
 
     public PaintSurface(StatesModel stmo) {
         stm = stmo;
@@ -27,6 +31,9 @@ class PaintSurface extends JComponent {
         this.addMouseListener(new MouseAdapter() {
             public void mousePressed(MouseEvent e) {
                 if ((e.getModifiersEx() & InputEvent.BUTTON1_DOWN_MASK) != 0) {// left click
+                    startDrag=e.getPoint();
+                    endDrag= new Point();
+                    endDrag.setLocation(startDrag.getX(),startDrag.getY());
                     leftClickListener(e);
                 } else if ((e.getModifiersEx() & InputEvent.BUTTON3_DOWN_MASK) != 0) { // right click
 
@@ -37,17 +44,52 @@ class PaintSurface extends JComponent {
 
             // 鼠标松开是可以创建一个矩形，将起始点归零
             public void mouseReleased(MouseEvent e) {
+                if (tmpDrawable != null) {
+                    stm.execute(new Command(){
+                        Drawable tmp = tmpDrawable;
+                        {
+                            tmp.setColor(stm.getColor());
+                            tmp.setAlpha(stm.getAlpha());
+                            tmp.setBorder(stm.getColor(), new BasicStroke()); //todo
+                            tmp.disableFill();
+                        }
+                        @Override
+                        public void execute() {
+                            stm.getAllDrawable().add(tmp);
+                        }
+
+                        @Override
+                        public void unexecute() {
+                            stm.getAllDrawable().remove(tmp);
+                        }
+                    });
+                    tmpDrawable = null;
+                }
+                
+                if(selectedTip != null){
+                    stm.execute(new Command(){
+                        Drawable selected = selectedDrawable;
+                        Point2D.Float selectedStart = selected.getStart();
+                        double deltax = endDrag.getX() - startDrag.getX();
+                        double deltay = endDrag.getY() - startDrag.getY();
+
+                        @Override
+                        public void execute() {
+                            selected.moveStartTo((float)(selectedStart.getX()+deltax),(float)(selectedStart.getY()+deltay));
+                        }
+                        
+                        @Override
+                        public void unexecute() {
+                            selected.moveStartTo((float)(selectedStart.getX()-deltax),(float)(selectedStart.getY()-deltay));
+                        }
+                    });
+                    selectedTip = null;
+                    selectedDrawable=null;
+                }
+
                 startDrag = null;
                 endDrag = null;
-                if (tmpDrawable != null) {
-                    tmpDrawable.setColor(stm.getColor());
-                    tmpDrawable.setAlpha(stm.getAlpha());
-                    tmpDrawable.setBorder(stm.getColor(), new BasicStroke()); //todo
-                    tmpDrawable.disableFill();
-                    stmo.getAllDrawable().add(tmpDrawable);
-                    tmpDrawable = null;
-                    repaint();
-                }
+                repaint();
             }
         });
 
@@ -58,11 +100,18 @@ class PaintSurface extends JComponent {
                 if (endDrag != null) {
                     int x = e.getX();
                     int y = e.getY();
-                    endDrag.move(x, y);
                     if(tmpDrawable!=null){
                         tmpDrawable.putEndPoint(x,y);
                         repaint();
                     }
+                    if(selectedTip!=null){
+                        var deltax = e.getX() - endDrag.getX();
+                        var deltay = e.getY() - endDrag.getY();
+                        var selectedStart = selectedTip.getStart();
+                        selectedTip.moveStartTo((float)(selectedStart.getX()+deltax),(float)(selectedStart.getY()+deltay));
+                        repaint();
+                    }
+                    endDrag.move(x, y);
                 }
 
             }
@@ -70,15 +119,15 @@ class PaintSurface extends JComponent {
     }
 
     void leftClickListener(MouseEvent e){
-        startDrag=endDrag=e.getPoint();
         try{
             createTmpDrawable();
             repaint();
         }catch(Exception ex){
-            startDrag=endDrag=null;
             switch(stm.getType()){
                 case SELECT:{
-                    
+                    selectedTip = getSelectedDrawable();
+                    if(selectedTip!=null) System.out.println("selected");
+                    repaint();
                 }
                      break;
                 default: 
@@ -88,8 +137,22 @@ class PaintSurface extends JComponent {
     }
 
 
-    final float[] dash1 = { 3.0f, 3.0f };
-
+    private Drawable getSelectedDrawable(){
+        var l = stm.getAllDrawable();
+        for (int i = l.size(); i-- > 0; ) {
+            var shape = l.get(i);
+            if(shape.pointOn(startDrag.x,startDrag.y)){
+                selectedDrawable = shape;
+                var s = shape.getStart();
+                var rec = BasicDrawableFactory.makeRec((int)s.x, (int)s.y, (int)s.x, (int)s.y);
+                rec.putEndPoint(shape.getEndPoint());
+                rec.setBorder(shape.getBorderColor(), new BasicStroke(3.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER, 10.0f, dash1, 3.0f));
+                return rec;
+            }
+        }
+        return null;
+    }
+    
     private void createTmpDrawable() {
         switch (stm.getType()) {
             case LINE: {
@@ -149,8 +212,8 @@ class PaintSurface extends JComponent {
     }
 
     // 初始化背景
-    private void paintBackground(Graphics2D g2) {
-        g2.setPaint(Color.LIGHT_GRAY);
+    private void paintBackground(Graphics2D g2) {//TODO: 改的有创意一点，比如类似PS的灰白相间代表透明
+        g2.setPaint(Color.WHITE);
 
         for (int i = 0; i < size.width; i += 10) {
             Shape line = new Line2D.Float(i, 0, i, size.height);
@@ -164,8 +227,7 @@ class PaintSurface extends JComponent {
     }
 
     private void paintBoard(Graphics2D g2) {
-        var a = stm.getAllDrawable();
-        for (var d : a) {
+        for (var d : stm.getAllDrawable()) {
             d.drawOnGraphics2D(g2);
         }
     }
@@ -190,6 +252,9 @@ class PaintSurface extends JComponent {
         }
         if (tmpDrawable != null) { // 提示对象的绘制
             tmpDrawable.drawOnGraphics2D(g2);
+        }
+        if (selectedTip != null) { //移动对象的绘制
+            selectedTip.drawOnGraphics2D(g2);
         }
     }
 
